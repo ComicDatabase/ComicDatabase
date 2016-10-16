fcc62fadf780e9b2fcc68ac913111f8068207a93
Comic DataBase <admin@comicdatabase.com>
2016-10-16 06:35:33 +0000

    2016-1016-06:35:33->545411--17

--- smp/downsmpmid.fish
+++ smp/downsmpmid.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 set mid $argv[1]
 #set mid 1056492
-for cidline in (wget -O- http://smp.yoedge.com/view/gotoAppLine/$mid | grep shinmangaplayer | sed -r 's/.*href=".*\/([0-9]+).*">([0-9]+)<.*/\2\t\1/' | grep -v '<a')
+for cidline in (wget -O- http://smp.yoedge.com/view/omnibus/$mid | grep shinmangaplayer | sed -r 's/.*href=".*\/([0-9]+).*">([0-9]+)<.*/\2\t\1/' | grep -v '<a')
 	set title (echo $cidline|sed -r 's/(.*)\t(.*)/\1/')	
 	set cid   (echo $cidline|sed -r 's/(.*)\t(.*)/\2/')
         echo $title $cid
